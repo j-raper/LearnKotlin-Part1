79761b0a470f118df9d708349372a5f287b1e7c5
JuliezaRaper <89051269+j-raper@users.noreply.github.com>
2022-08-26 22:35:08 +0800

    Update Collection.kt

--- Collection.kt
+++ Collection.kt
@@ -1,1 +1,9 @@
+// List
+
+// Creating an Immutable List
+fun main() {
+  var waterSports = listOf("Wind Surfing", "Sailing", "Swimming", "Jet Skiing", "Water Skiing")
+
+  println(waterSports)
+}
 
