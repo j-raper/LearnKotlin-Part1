ae281a6c905d75d00d3ae1c1c3b3a1e7cefebf94
JuliezaRaper <89051269+j-raper@users.noreply.github.com>
2022-08-26 12:25:19 +0800

    Update Introduction.kt

--- Introduction.kt
+++ Introduction.kt
@@ -1,6 +1,39 @@
-//First Kotlin Program
+// First Kotlin Program
 
-//MainFunction
+// MainFunction
 fun main() {
   println("Coding is fun!")
 }
+
+// Print Function
+fun main() {
+  println("Just keep swimming!")
+  print(15 * 4)
+}
+
+// Order of Execcution
+fun main() {
+  println("11/2 - AU military goes to war against local emus.")
+  println("11/4 - Ambush planned by military fails.")
+  println("11/8 - AU military withdraws."
+}
+
+// Comments
+fun main() {
+  println("Howdy, partner!") 
+  //Code prints "Howdy, partner!" to the terminal.
+  /*The things that I know so far about Kotlin are the main function, print statements, the order of execution, lastly, the comments.*/
+}
+
+// Error or bugs
+fun main() {
+  println("An error a day keeps the programmer away.")
+}
+
+/*
+  - Kotlin is a general-purpose programming language.
+  - The main() function is the starting point of all Kotlin programs.
+  - When we want to output something to the terminal, we can use print statements like println() or print().
+  - Code is read from top to bottom.
+  - Comments are used to explain what is happening in our code.  
+*/
